7f045d3c2ab76251b160196c766dfb6a0fefeffa
Matchobas <maalexandria@hotmail.com>
2022-06-18 22:05:59 -0300

    Replying to message

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import 'dotenv/config';
 import { Client, Intents } from 'discord.js';
 
+const intents = new Intents();
+intents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILDS);
 const client = new Client({
-  intents: Intents.FLAGS.GUILDS,
+  intents: intents,
+});
+
+client.on('ready', () => {
+  console.log(`Logged in`);
+});
+
+client.on('messageCreate', msg => {
+  if (msg.content === 'hey') {
+    msg.channel.send('Hi my dud');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
