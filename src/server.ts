8b49d9e253ff7903388199e5ea4b8e522436a9ad
Matchobas <maalexandria@hotmail.com>
2022-06-19 23:56:35 -0300

    Sending QR code correctly

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Client, Intents } from 'discord.js';
 import axios from 'axios';
+import { config } from 'dotenv';
 
 const intents = new Intents();
 intents.add(
@@ -20,9 +21,12 @@ client.on('messageCreate', msg => {
   if (msg.content === 'hey') {
     let qrcode;
     axios
-    .get(`https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=${msg.content}`)
+    .get(`https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=https://github.com/Matchobas`)
     .then(res => {
       console.log(`statusCode: ${res.status}`);
+      if(res.config.url) {
+        msg.reply(res.config.url);
+      }
     })
     .catch(error => {
       console.log(error);
