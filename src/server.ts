7e3f19c160efe44f9d11c8b13580fd1cafce7efe
Matchobas <maalexandria@hotmail.com>
2022-06-19 23:50:15 -0300

    Using axios to connect with google API

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 import { Client, Intents } from 'discord.js';
+import axios from 'axios';
 
 const intents = new Intents();
-intents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILDS);
+intents.add(
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILDS,
+);
 const client = new Client({
   intents: intents,
 });
@@ -13,7 +18,15 @@ client.on('ready', () => {
 
 client.on('messageCreate', msg => {
   if (msg.content === 'hey') {
-    msg.channel.send('Hi my dud');
+    let qrcode;
+    axios
+    .get(`https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=${msg.content}`)
+    .then(res => {
+      console.log(`statusCode: ${res.status}`);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 });
 
